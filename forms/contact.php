aca28ef94558b0660467e92f086789a10b184c33
calamarit <94392801+calamarit@users.noreply.github.com>
2021-11-23 17:45:21 -0300

    update.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Felicidades</title>
+</head>
+<body>
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+  $destinatario = 'federicoarroyo88@gmail.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'calamarit.com@gmail.com';
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  $mensaje = "
+        Nombre: ".$name."
+        Correo: ".$email."
+        Mensaje: ".$message."
+  ";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  mail($destinatario,utf8_decode($mensaje));
+  echo "<h2>Hemos recibido tu mensaje, pronto te contestaremos gracias.</h2>";
+
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
+
+</body>
+</html>
